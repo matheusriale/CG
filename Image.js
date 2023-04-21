97c0506e3532c9fafc1fea126cb1045dfe3bf44d
caiofov <cfoviana@gmail.com>
2023-04-21 20:04:32 -0300

    classe ImageCG

---
+++ Image.js
@@ -0,0 +1,16 @@
+class ImageCG {
+    /**
+     * Cria uma nova imagem
+     * @param {Number} width Largura
+     * @param {Number} height Altura
+     * @param {Number} background Intensidade do fundo da nova Imagem (padrão: 255)
+     */
+    constructor(width, height, background = 255) {
+        // this.pixels = screen_pixels
+        this.width = width
+        this.height = height
+        this.background = background
+
+    }
+
+}
