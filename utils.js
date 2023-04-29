3772dad8a07e6270422ba1be4751b1f471b02190
caiofov <cfoviana@gmail.com>
2023-04-29 19:19:18 -0300

    função de calcular idx do p5 como método de Pixel

--- utils.js
+++ utils.js
@@ -1,15 +1,4 @@
 /**
- * Dadas as coordenadas x e y, retorna o índice do pixel no array do P5.js
- * @param {Number} x 
- * @param {Number} y 
- * @returns {Number} índice do pixel no array de pixels do P5.js
- */
-function pixel_idx(x, y) {
-    return 4 * (y * width + x)
-}
-
-
-/**
  * Estrutura para localização de pixel
  * @param {Number} x Coordenada X do pixel
  * @param {Number} y Coordenada Y do pixel
@@ -28,6 +17,13 @@ function Pixel(x, y) {
      */
     this.to_array = () => {
         return [this.x, this.y]
+    }
+    /**
+     * Retorna o índice do pixel no array do P5.js
+     * @returns {Number} índice do pixel no array de pixels do P5.js
+     */
+    this.get_idx = () => {
+        return 4 * (this.y * width + this.x)
     }
 }
 
