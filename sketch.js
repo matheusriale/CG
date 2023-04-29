730890e2a2c65029bc8d630a3a1761745009d936
caiofov <cfoviana@gmail.com>
2023-04-29 18:36:12 -0300

    fix: declaração de variáveis e chamada de métodos

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 var img
 
 function setup() {
-  img = new ImageCG(400, 400, 0)
+  img = new ImageCG(100, 100, 200)
   img.init()
+
+  img.reta(51, 50, 100, 150)
+  img.set_pixel(52, 50)
+  img.set_pixel(53, 50)
 }
 
 function draw() {
