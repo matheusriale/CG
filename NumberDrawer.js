96f440f7dd6db2b5c99e05bbc802dc15f1022bb0
caiofov <cfoviana@gmail.com>
2023-04-30 18:39:03 -0300

    desenhar vários algarismos

--- NumberDrawer.js
+++ NumberDrawer.js
@@ -6,25 +6,32 @@ class NumberDrawer {
      */
     constructor(font_size, spacing) {
         this.font_size = font_size || 20
-        this.spacing = spacing || 5
+        this.spacing = spacing || 10
     }
 
     /**
      * Desenha um número na tela
-     * @param {Number} num número a ser desenhado
+     * @param {String} num número a ser desenhado
      * @param {Pixel} pos posição inicial do número
      * @returns {Array<Array<Pixel>>} Figuras a serem desenhadas (uma para cada dígito)
      */
     get_number(num, pos) {
+        let digits = num.split("")
         let points = []
-        switch (num) {
-            case 1:
-                points.push(this.one(pos))
-                break;
+        for (let i = 0; i < digits.length; i++) {
+            const d = digits[i];
+            let curr_pos = new Pixel(pos.x + this.spacing * i, pos.y)
+            switch (d) {
+                case '1':
+                    points.push(this.one(curr_pos))
+                    break;
 
-            default:
-                break;
+                default:
+                    break;
+            }
         }
+
+
         return points
     }
     /**
--- sketch.js
+++ sketch.js
@@ -5,12 +5,13 @@ var num
 function preload() {
   img = new ImageCG(100, 100, 200)
   let drawer = new NumberDrawer()
-  num = drawer.get_number(1, new Pixel(10))[0]
+  num = drawer.get_number("11", new Pixel(10))
 }
 
 function setup() {
   img.init()
-  img.draw_figure(num, 255)
+  img.draw_figure(num[0], 255)
+  img.draw_figure(num[1], 255)
   noLoop()
 }
 
