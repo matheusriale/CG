8238b8dee5109e4c62b7bc12000f7b46efe8276d
caiofov <cfoviana@gmail.com>
2023-04-30 16:25:21 -0300

    formatting

--- sketch.js
+++ sketch.js
@@ -5,10 +5,10 @@ function setup() {
   img.init()
 
   var pol = new Polygon(255)
-  pol.add_vertex([new Pixel(10,10), new Pixel(10,90), new Pixel(90,90), new Pixel(90,10)])
-  
+  pol.add_vertex([new Pixel(10, 10), new Pixel(10, 90), new Pixel(90, 90), new Pixel(90, 10)])
+
   img.draw_polygon(pol, true)
-  }
+}
 
 function draw() {
 
