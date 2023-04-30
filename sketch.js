eaca6bb0b4d983a76380c27446cca93f97280ea7
caiofov <cfoviana@gmail.com>
2023-04-30 17:34:57 -0300

    html bonito

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 var img
+var pol
+
+function preload() {
+  img = new ImageCG(100, 100, 200)
+  pol = new Polygon(255)
+  pol.add_vertex([new Pixel(10, 10), new Pixel(10, 90), new Pixel(90, 90), new Pixel(90, 10)])
+}
 
 function setup() {
-  img = new ImageCG(100, 100, 200)
   img.init()
-
-  var pol = new Polygon(255)
-  pol.add_vertex([new Pixel(10, 10), new Pixel(10, 90), new Pixel(90, 90), new Pixel(90, 10)])
-
   img.draw_polygon(pol, true)
+  noLoop()
 }
 
 function draw() {
