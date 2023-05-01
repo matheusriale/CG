fe2c1f46be7a5568a16cd54fbe4e3007ba8444d0
caiofov <cfoviana@gmail.com>
2023-04-30 23:07:24 -0300

    mais params Clock

--- Clock.js
+++ Clock.js
@@ -1,9 +1,16 @@
+/**
+ * @type {{num_drawer: NumberDrawer center: Pixel radius: Number}}
+ */
 class Clock {
     /**
      * Constrói um relógio
      * @param {NumberDrawer} num_drawer 
+     * @param {Pixel} center Coordenadas do centro do relógio
+     * @param {Number} radius Tamanho do raio
      */
-    constructor(num_drawer) {
+    constructor(num_drawer, center, radius) {
         this.num_drawer = num_drawer
+        this.center = center
+        this.radius = radius
     }
 }
