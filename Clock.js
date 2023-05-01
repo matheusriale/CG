3a227a2fb3bc0e548f003acca399c08917060b7e
caiofov <cfoviana@gmail.com>
2023-04-30 23:52:16 -0300

    relogio feio gambiarra

--- Clock.js
+++ Clock.js
@@ -13,4 +13,39 @@ class Clock {
         this.center = center
         this.radius = radius
     }
+
+    get_hour_numbers() { //TODO: gambiarra
+        let nums = []
+        let height = this.radius * 2
+        let init_pos = new Pixel(this.center.x, this.center.y - this.radius)
+
+
+        for (let i = 1; i <= 3; i++) {
+            let pos = new Pixel(
+                init_pos.x + this.radius * i / 4,
+                init_pos.y + height * i / 8)
+            nums.push(...this.num_drawer.get_number(i.toString(), pos))
+        }
+        for (let i = 4; i <= 6; i++) {
+            let pos = new Pixel(
+                init_pos.x + this.radius * (7 - i) / 4,
+                init_pos.y + height * (i) / 8)
+            nums.push(...this.num_drawer.get_number(i.toString(), pos))
+        }
+
+        for (let i = 7; i <= 9; i++) {
+            let pos = new Pixel(
+                init_pos.x + this.radius * (7 - i) / 4,
+                init_pos.y + height * (13 - i) / 8)
+            nums.push(...this.num_drawer.get_number(i.toString(), pos))
+        }
+
+        for (let i = 10; i <= 12; i++) {
+            let pos = new Pixel(
+                init_pos.x + this.radius * (13 - i) / 4,
+                init_pos.y + height * (12 - i) / 8)
+            nums.push(...this.num_drawer.get_number(i.toString(), pos))
+        }
+        return nums
+    }
 }
