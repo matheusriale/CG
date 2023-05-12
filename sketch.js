9ec3861c8dc3379e5c8910ea3b37f1aa4aa5f572
Matheus Ribeiro <matheusriale@alu.ufc.br>
2023-05-12 19:10:58 -0300

    get_pixel_tex

--- sketch.js
+++ sketch.js
@@ -7,15 +7,22 @@ var p
 var l
 var ang = 0
 
+var imgteste;
 function preload() {
   img = new ImageCG(100, 100, 200)
+
+  imgteste = loadImage('images/imgteste.jpg')
 }
 
 function setup() {
-  img.init()
-  c = new Pixel(50, 50)
-  p = new Pixel(0, 50)
-  l = new Line(c, p, 100)
+  // img.init()
+  // c = new Pixel(50, 50)
+  // p = new Pixel(0, 50)
+  // l = new Line(c, p, 100)
+
+  //Teste tex de img
+  image(imgteste,0,0)
+  console.log(img.get_pixel_tex(new Pixel(4,4),imgteste))
 }
 
 function draw() {
@@ -23,7 +30,8 @@ function draw() {
   // img.reta(new_l.pi, new_l.pf, new_l.stroke_intensity)
   // new_l.stroke_intensity = img.background
   // img.draw_figure(new_l)
-  ang = ang >= 360 ? 0 : ang + 10
+  // ang = ang >= 360 ? 0 : ang + 10
+  
 
   // img.clear(200)
 }
