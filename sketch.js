1d97702fc7947d3bb8cd316e899635d15438c5c7
caiofov <cfoviana@gmail.com>
2023-05-15 22:29:53 -0300

    atualizar a main

--- sketch.js
+++ sketch.js
@@ -1,38 +1,21 @@
 var img
 var clk
-var num
 
-var c
-var p
-var l
-var ang = 0
-
-var imgteste;
 function preload() {
   img = new ImageCG(100, 100, 200)
-
-  //imgteste = loadImage('images/imgteste.jpg')
+  clk = new Clock(new NumberDrawer(), new Pixel(50), 40)
 }
 
 function setup() {
-  // img.init()
-  // c = new Pixel(50, 50)
-  // p = new Pixel(0, 50)
-  // l = new Line(c, p, 100)
-
-  //Teste tex de img
-  
-  // image(imgteste,0,0)
-  // console.log(img.get_pixel_tex(new Pixel(4,4),imgteste))
+  img.init()
 }
 
 function draw() {
-  // let new_l = l.rotate(ang)
-  // img.reta(new_l.pi, new_l.pf, new_l.stroke_intensity)
-  // new_l.stroke_intensity = img.background
-  // img.draw_figure(new_l)
-  // ang = ang >= 360 ? 0 : ang + 10
-  
 
-  // img.clear(200)
+  clk.update()
+  clk.get_hands().forEach(h => {
+    img.draw_figure(h)
+  })
+
+  setTimeout(() => { img.clear(200); }, 1000); //esperar para limpar a tela
 }
