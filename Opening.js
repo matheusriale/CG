602a93c655174548feeccb1ff686866b4c4013c2
caiofov <cfoviana@gmail.com>
2023-05-22 23:40:23 -0300

    elipse tela de abertura

--- Opening.js
+++ Opening.js
@@ -11,23 +11,34 @@ class Opening {
         this.screen = screen
         this.is_runing = false
         this._char_drawer = char_drawer
+        this.init_pixel = new Pixel(20, 20)
+        this._elipse_color = 0
+        this.font_size = this._char_drawer.font_size
+        this.padding = 10
     }
 
     start() {
         this.is_runing = true
+        this._draw_elipse()
         this._draw_C()
         this._draw_G()
     }
 
     _draw_C() {
-        let c_init = new Pixel(10)
+        let c_init = this.init_pixel
         this.screen.draw_figure(this._char_drawer.letter_C(c_init, 3))
-        this.screen.floodFill(c_init.copy().add(new Pixel(2)), this._char_drawer.stroke)
+        this.screen.floodFill(c_init.copy().add(new Pixel(2)), this._elipse_color)
     }
     _draw_G() {
-        let g_init = new Pixel(40, 10)
+        let g_init = this.init_pixel.copy().add(new Pixel(this.font_size + this.padding, 0))
         this.screen.draw_figure(this._char_drawer.letter_G(g_init, 3))
-        this.screen.floodFill(g_init.copy().add(new Pixel(2)), this._char_drawer.stroke)
+        this.screen.floodFill(g_init.copy().add(new Pixel(2)), this._elipse_color)
+    }
+
+    _draw_elipse() {
+        let center = this.init_pixel.copy().add(new Pixel(this.font_size + this.padding / 2, this.font_size / 2))
+        this.screen.ellipse(center, this.font_size * 2, 20, this._elipse_color)
+        this.screen.floodFill(center, this._elipse_color)
     }
 
     stop() {
