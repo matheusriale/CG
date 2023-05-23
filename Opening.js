8e20b38ba456e94061b7891099381ed496221910
caiofov <cfoviana@gmail.com>
2023-05-22 23:26:55 -0300

    desenhar tela de abertura

--- Opening.js
+++ Opening.js
@@ -15,10 +15,23 @@ class Opening {
 
     start() {
         this.is_runing = true
-        this.screen.draw_figure(this._char_drawer.letter_C(new Pixel(10)))
+        this._draw_C()
+        this._draw_G()
+    }
+
+    _draw_C() {
+        let c_init = new Pixel(10)
+        this.screen.draw_figure(this._char_drawer.letter_C(c_init, 3))
+        this.screen.floodFill(c_init.copy().add(new Pixel(2)), this._char_drawer.stroke)
+    }
+    _draw_G() {
+        let g_init = new Pixel(40, 10)
+        this.screen.draw_figure(this._char_drawer.letter_G(g_init, 3))
+        this.screen.floodFill(g_init.copy().add(new Pixel(2)), this._char_drawer.stroke)
     }
 
     stop() {
         this.is_runing = false
+        this.screen.clear()
     }
 }
