5b2119180a80c30dd9105a86d6f81a5e2e090bbd
caiofov <cfoviana@gmail.com>
2023-05-23 18:35:42 -0300

    preenchimento e cores na tela de abertura

--- Opening.js
+++ Opening.js
@@ -17,9 +17,12 @@ class Opening {
         this._border_color = new Color(255, 255, 0)
         this.font_size = this._char_drawer.font_size
         this.padding = 10
+        this.font_weight = 3.5
+        this.font_color = new Color(255)
     }
 
     start() {
+        console.log("Iniciando tela de abertura...")
         this.is_running = true
         this._draw_elipse()
         this._draw_C()
@@ -28,34 +31,41 @@ class Opening {
     }
 
     _draw_C() {
+        console.log("C")
         let c_init = this.init_pixel
-        this.screen.draw_figure(this._char_drawer.letter_C(c_init, 3))
-        this.screen.floodFill(c_init.copy().add(new Pixel(2)), this._elipse_color)
+        this.screen.draw_figure(this._char_drawer.letter_C(c_init, this.font_weight))
+        let flood_init = c_init.copy().add(new Pixel(this.font_weight - 1))
+        this.screen.floodFill(flood_init, this.font_color, this._elipse_color, true)
     }
     _draw_G() {
+        console.log("G")
         let g_init = this.init_pixel.copy().add(new Pixel(this.font_size + this.padding, 0))
-        this.screen.draw_figure(this._char_drawer.letter_G(g_init, 3))
-        this.screen.floodFill(g_init.copy().add(new Pixel(2)), this._elipse_color)
+        this.screen.draw_figure(this._char_drawer.letter_G(g_init, this.font_weight))
+        let flood_init = g_init.copy().add(new Pixel(this.font_weight - 1))
+        this.screen.floodFill(flood_init, this.font_color, this._elipse_color)
     }
 
     _draw_elipse() {
+        console.log("Elipse")
         let center = this.init_pixel.copy().add(new Pixel(this.font_size + this.padding / 2, this.font_size / 2))
         this.screen.ellipse(center, this.font_size * 2, 20, this._border_color)
         this.screen.floodFill(center, this._elipse_color)
     }
 
     stop() {
+        console.log("Encerrando tela inicial...")
         this.is_running = false
         this.screen.clear()
     }
 
     _draw_start_button() {
+        console.log("Botão")
         let center = new Pixel(45, 70)
         this.screen.circumference(center, 10, this._border_color)
         this.screen.floodFill(center, this._button_color)
 
         let triangle = Polygon.triangle(center, 5, 255)
         this.screen.draw_figure(triangle)
-        this.screen.floodFill(center.copy().sub(new Pixel(3)), 255, this._elipse_color)
+        this.screen.floodFill(center.copy(), new Color(255), this._elipse_color)
     }
 }
