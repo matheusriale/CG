3ef5808dd1c136cf4fba79878049bfd1beabfda2
Matheus Ribeiro <matheusriale@alu.ufc.br>
2023-05-26 00:18:50 -0300

    img para matriz de pixels no sketch testes

--- ImageCG.js
+++ ImageCG.js
@@ -116,10 +116,10 @@ class ImageCG {
     }
     let x = Math.round(p.xtex * (tex.width - 1) + 1);
     let y = Math.round(p.ytex * (tex.height - 1) + 1);
-
-    let intensity = tex.get(x, y);
-    console.log(p.xtex)
-    return intensity;
+    
+    //let intensity = tex.get(x, y);
+    
+    //return intensity;
   }
 
 
@@ -335,6 +335,7 @@ class ImageCG {
 
     // Segmento horizontal -> sem intersecao
     if (pi.y == pf.y) {
+      
       let p = new Pixel(-1, 0, 0, 0); //ptex = [x,y,xtex,ytex]
       return [p];
     }
@@ -345,12 +346,15 @@ class ImageCG {
 
     // Calcula t
     var t = (y - pi.y) / (pf.y - pi.y);
+    
 
     // Calcula x
     if (t > 0 && t <= 1) {
       var x = pi.x + t * (pf.x - pi.x);
       let tx = pi.xtex + t * (pf.xtex - pi.xtex);
-      let ty = pi.ytex + t * (pf.ytex - pf.ytex);
+      let ty = pi.ytex + t * (pf.ytex - pi.ytex);
+      
+      //console.log(tx)
 
       return [new Pixel(x, y, tx, ty), t]
     }
@@ -363,8 +367,10 @@ class ImageCG {
   reta_tex(pi, pf, tex, clg = false) {
     let [dx, dy] = Pixel.distance(pi, pf)
     let passos = max(Math.abs(dy), Math.abs(dx))
+    //console.log(passos)
 
     if (passos == 0) {
+      
       let intensity = this.get_pixel_tex(pi, tex)
       this.set_pixel(pi, intensity)
       return
@@ -372,24 +378,34 @@ class ImageCG {
 
     let passo_x = dx / passos;
     let passo_y = dy / passos;
+    //console.log(passo_x)
 
     for (let i = 0; i < passos; i++) {
       let is_one = Math.abs(Math.round(passo_x)) == 1
 
       let x = pi.x + i * passo_x;
       let y = pi.y + i * passo_y;
+      let pc = (x - pi.x)/(pf.x - pi.x);
+      
+      let tx = pi.xtex + pc*(pf.xtex - pi.xtex);
+      let ty = pi.ytex + pc*(pf.ytex - pi.ytex);
+      
+      let intensidade = this.get_pixel_tex(tex, tx, ty);
+      
+      //img = setpixel(img, xk, y, intensidade);
 
-      if (is_one) {
-        var px1 = new Pixel(Math.round(x), Math.floor(y))
-        var px2 = new Pixel(Math.round(x), Math.floor(y + 1))
+      if (is_one) {// adicionar texturas
+        var px1 = new Pixel(Math.round(x), Math.floor(y),tx,ty)
+        var px2 = new Pixel(Math.round(x), Math.floor(y + 1),tx,ty)
       }
       else {
-        var px1 = new Pixel(Math.floor(x), Math.round(y))
-        var px2 = new Pixel(Math.floor(x + 1), Math.round(y))
+        var px1 = new Pixel(Math.floor(x), Math.round(y),tx,ty)
+        var px2 = new Pixel(Math.floor(x + 1), Math.round(y),tx,ty)
       }
-
+      //console.log(px1)
       this.set_pixel(px1, this.get_pixel_tex(px1, tex));
       this.set_pixel(px2, this.get_pixel_tex(px2, tex));
+      
     }
 
     if (clg) {
@@ -413,11 +429,14 @@ class ImageCG {
         //4vezes
         var pf = pol.vertices[p];
         var pint = this.intersection(y, new Line(pi, pf))[0]; // segmento válido
+        
         if (pint.x >= 0) {
           for (let k = 0; k < pol.vertices.length; k++) {
             var pint2 = this.intersection(y, new Line(pf, pol.vertices[k]))[0];//[0]-> pixel [1]-> t
             if (pint2.x >= 0) {
               this.reta_continua(pint2, pint);
+              
+              
             }
           }
         }
@@ -531,11 +550,15 @@ class ImageCG {
         //4vezes
         var pf = pol.vertices[p];
         var pint = this.intersection_tex(y, new Line(pi, pf))[0]; // segmento válido
+        //console.log(pint)
         if (pint.x >= 0) {
           for (let k = 0; k < pol.vertices.length; k++) {
             var pint2 = this.intersection_tex(y, new Line(pf, pol.vertices[k]))[0];//[0]-> pixel [1]-> t
+            //console.log(pint2.xtex)
             if (pint2.x >= 0) {
               this.reta_tex(pint2, pint, tex);
+              
+              
             }
           }
         }
--- sketch.js
+++ sketch.js
@@ -1,39 +1,72 @@
-var img, opening
-var clk
-
+var img, opening;
+var clk;
 
 function preload() {
   img = new ImageCG(100, 100, 200)
-  opening = new Opening(img, new CharDrawer())
+  // opening = new Opening(img, new CharDrawer())
+  // imgteste = loadImage('images/imgteste.jpg')
   imgteste = loadImage('images/imgteste.jpg')
-  clk = new Clock(new CharDrawer(155, 10, 10), new Pixel(50), 50)
+
+
+    //Funcao para pegar a matriz de pixeis da imagem:
+  image(imgteste, 0, 0);
+  loadPixels()
+  image_matrix = []
+  matrix_line = []
+  for (var y = 0; y< height; y++){
+    for (var x = 0; x < width; x++){
+      var index = (x+y*width)*4;
+      matrix_line.push(pixels[index],pixels[index+1],pixels[index+2],pixels[index+3])
+    }
+    image_matrix.push(matrix_line)
+    matrix_line = []
+  }
+  console.log(image_matrix)
+  // clk = new Clock(new CharDrawer(155, 10, 10), new Pixel(50), 50)
 }
 
 function setup() {
+  
   img.init()
-  let p = new Polygon(0, [pix2 = new Pixel(10, 10, 1, 0), pix3 = new Pixel(60, 10, 0, 0), pix4 = new Pixel(60, 60, 0, 0), pix5 = new Pixel(70, 45, 0, 0), pix6 = new Pixel(70, 5, 1, 0), pix7 = new Pixel(25, 5, 0, 0)])
-  img.set_pixel_color(pix2, new Color(0, 255, 0), clg = true);
-  img.set_pixel_color(pix3, new Color(0, 0, 255), clg = true);
 
-  img.reta_continua_gradient(pix2, pix3)
-  // img.set_pixel_color(pix4, new Color(255, 0, 255), clg = true);
-  // img.set_pixel_color(pix5, new Color(0, 255, 255), clg = true);
-  // img.set_pixel_color(pix6, new Color(0, 255, 0), clg = true);
-  // img.set_pixel_color(pix7, new Color(255, 0, 255), clg = true);
-
-  // img.draw_figure(p, 0, true);
-
-  // img.scanline_gradient(p);
-  // opening.start()
-}
-
-function mousePressed() {
-  if (opening.is_runing) {
-    opening.stop()
+  //Funcao para pegar a matriz de pixeis da imagem:
+  image(imgteste, 0, 0);
+  loadPixels()
+  image_matrix = []
+  matrix_line = []
+  for (var y = 0; y< height; y++){
+    for (var x = 0; x < width; x++){
+      var index = (x+y*width)*4;
+      matrix_line.push(pixels[index],pixels[index+1],pixels[index+2],pixels[index+3])
+    }
+    image_matrix.push(matrix_line)
+    matrix_line = []
   }
+  console.log(image_matrix)
+  //opening.start()
+  clear()
+
+  
+
+  img2 = new ImageCG(100, 100, 200)
+  img2.init()
+
+  p = new Polygon(0,[pix1 = new Pixel(10,10,0,0),pix2 = new Pixel(10,60,1,0),pix3 = new Pixel(60,60,1,1),pix4 = new Pixel(60,10,0,1)])
+  img.draw_figure(p,0);
+  img.scanline_tex(p,imgteste);
+
 }
+// function mousePressed() {
+//   if (opening.is_runing) {
+//     opening.stop()
+//   }
+// }
 
 function draw() {
+  
+  // image(imgteste,0,0)
+  // console.log(imgteste.get(50,50))
+  
   // if (opening.is_runing) return
 
   // // Relogio
@@ -47,4 +80,6 @@ function draw() {
 
   // setTimeout(() => { img.clear(); }, 1000); //esperar para limpar a tela
 
+
 }
+
