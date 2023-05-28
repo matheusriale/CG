8277d0de6bdef0c9e68a15f414b30317adb2a861
caiofov <cfoviana@gmail.com>
2023-05-28 20:38:19 -0300

    Renomear window -> windowCG

--- View.js
+++ View.js
@@ -4,7 +4,7 @@
  * @param {Pixel} end 
  * @type {{start:Pixel end:Pixel}}
  */
-function Window(start, end) {
+function WindowCG(start, end) {
     this.start = start
     this.end = end
     this.diff_x = (this.end.x - this.start.x)
