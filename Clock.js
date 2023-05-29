2d3e9714b80c8487a117d0ffd9cd39beb0f0cb0e
caiofov <cfoviana@gmail.com>
2023-05-29 03:17:09 -0300

    remover função de numeros

--- Clock.js
+++ Clock.js
@@ -11,7 +11,7 @@ class Clock {
      * @param {Number} radius Tamanho do raio
      * @param {ImageCG} screen
      */
-    constructor(num_drawer, center, radius, screen, border = 0,image) {
+    constructor(num_drawer, center, radius, screen, border = 0, image) {
         this.num_drawer = num_drawer
         this.center = center
         this.radius = radius
@@ -22,13 +22,11 @@ class Clock {
         this.image = image
 
         this.reset_hands()
-        
+
         this.border = new Circumference(border, this.center.copy(), this.radius)
         this.top = this.center.sub(new Pixel(this.radius))
         this.bottom = this.center.add(new Pixel(this.radius))
-        this.p = Polygon.rect(this.top.copy(),this.bottom.copy())
-        this.screen.draw_figure(this.p)
-        
+        this.p = Polygon.rect(this.top.copy(), this.bottom.copy())
     }
 
     /**
@@ -79,17 +77,12 @@ class Clock {
         this._update_second_hand()
         this._update_minute_hand()
         this._update_hour_hand()
-        
 
         for (const hand of this.get_hands()) {
             this.screen.draw_figure(hand)
         }
-        
-        this.screen.scanline_tex(this.p,this.image)
-        
-        
 
-
+        this.screen.scanline_tex(this.p, this.image)
     }
 
     scale(scale) {
@@ -116,8 +109,7 @@ class Clock {
 
         this.top = this.top.map_window(viewport, win)
         this.bottom = this.bottom.map_window(viewport, win)
-        this.p = this.p.map_window(viewport,win)
-       
+        this.p = this.p.map_window(viewport, win)
     }
 
     get_all_elements() {
@@ -132,21 +124,6 @@ class Clock {
             this.top,
             this.bottom
         ]
-    }
-
-    get_hour_numbers() { //TODO: gambiarra
-        let nums = []
-        let mid_height = this.center.y - this.num_drawer.font_size / 2
-        let pos_3 = new Pixel(this.center.x + this.radius - this.num_drawer.font_size, mid_height)
-        let pos_6 = new Pixel(this.center.x, this.center.y + this.radius - this.num_drawer.font_size * 1.1)
-        let pos_9 = new Pixel(this.center.x - this.radius + this.num_drawer.font_size * 2 / 4, mid_height)
-        let pos_12 = new Pixel(this.center.x - this.num_drawer.font_size * 2 / 4, this.center.y - this.radius)
-
-        nums.push(...this.num_drawer.get_number("12", pos_12))
-        nums.push(...this.num_drawer.get_number("3", pos_3))
-        nums.push(...this.num_drawer.get_number("6", pos_6))
-        nums.push(...this.num_drawer.get_number("9", pos_9))
-        return nums
     }
 
     /**
