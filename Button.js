d4792ec9c3fd07f651d9883dd532fef670622c15
caiofov <cfoviana@gmail.com>
2023-05-30 00:47:28 -0300

    hover do botao de voltar

--- Button.js
+++ Button.js
@@ -13,20 +13,27 @@ function Button(pos, size, image_name, screen) {
 
     this.is_hover = () => {
         return mouseX <= (this.pos.x + this.size.x) &&
-            mouseX >= (this.pos.x - this.size.x) &&
+            mouseX >= (this.pos.x) &&
             mouseY <= this.pos.y + this.size.y &&
-            mouseY >= this.pos.y - this.size.y
+            mouseY >= this.pos.y
     }
 
     this.update = () => {
-        if (this.is_hover && !this._last_hover) {
+        if (this.is_hover() && !this._last_hover) {
+            console.log(this.pos, this.size)
             cursor(HAND)
             this._last_hover = true
             this.screen.draw_figure(this.polygon)
         }
-        else if (!this.is_hover && this._last_hover) {
+        else if (!this.is_hover() && this._last_hover) {
             cursor(ARROW)
             this._last_hover = false
+            this.clear_area()
+            this.draw()
         }
+    }
+    this.clear_area = () => {
+        let margin = new Pixel(2)
+        this.screen.clear_area(this.polygon.vertices[0].sub(margin), this.polygon.vertices[3].add(margin))
     }
 }
