c4e4a032b668e15b127408ceb5bbe4deef27b376
caiofov <cfoviana@gmail.com>
2023-05-29 23:41:39 -0300

    ajustes sketch

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var zoomed_out = true
 var clocks_draw = []
 
 function preload() {
-  img = new ImageCG(120, 120, 200)
+  img = new ImageCG(100, 100, 200)
   imgteste = loadImage('images/relogio2.png')
 
   opening = new Opening(img, new CharDrawer())
@@ -13,22 +13,17 @@ function preload() {
 function setup() {
   img.init();
   imgteste.loadPixels()
-  let p = Polygon.square(new Pixel(50), imgteste.width)
-  console.log(imgteste.pixels)
-  img.scanline_tex(p, imgteste)
-  img.draw_figure(p, 50)
 
-  // viewport = new Viewport(width, height)
-  // window_cg = new WindowCG(new Pixel(0), new Pixel(50))
+  viewport = new Viewport(width, height)
+  window_cg = new WindowCG(new Pixel(0), new Pixel(50))
 
-  // clk1 = new Clock(new CharDrawer(155, 3, 10), new Pixel(40, 30), 25, img, 0, image_matrix)
-  // clk2 = new Clock(new CharDrawer(155, 3, 10), new Pixel(50, 75), 25, img, 0, image_matrix)
-  // clk3 = new Clock(new CharDrawer(155, 3, 10), new Pixel(75, 25), 25, img, 0, image_matrix)
-  // clk1.update()
-  // clocks = [clk1]
-  // clocks_draw = [...clocks]
+  clk1 = new Clock(new CharDrawer(155, 3, 10), new Pixel(20, 30), 15, img, 0, imgteste)
+  clk2 = new Clock(new CharDrawer(155, 3, 10), new Pixel(80, 30), 15, img, 0, imgteste)
+  clk3 = new Clock(new CharDrawer(155, 3, 10), new Pixel(50, 70), 15, img, 0, imgteste)
+  clocks = [clk1, clk2, clk3]
+  clocks_draw = [...clocks]
 
-  //opening.start()
+  opening.start()
 }
 
 function mousePressed() {
@@ -57,7 +52,6 @@ function mousePressed() {
 
 
 function draw() {
-  return
   if (opening.is_running) {
     opening.update()
     return
@@ -65,27 +59,4 @@ function draw() {
   for (const clock of clocks_draw) {
     clock.update()
   }
-}
-
-//Funcao para pegar a matriz de pixeis da imagem:
-function image_pixels(imgteste, w, h) {
-  image(imgteste, 0, 0);
-  loadPixels()
-  image_matrix = []
-  matrix_line = []
-
-  for (var y = 0; y < w; y++) {
-    for (var x = 0; x < h; x++) {
-      var index = (x + y * width) * 4;
-      if (pixels[index] == 255 && pixels[index + 1] == 255 && pixels[index + 2] == 255) {
-        pixels[index + 3] = 0;
-      }
-      matrix_line.push(...pixels.slice(index, index + 4))
-    }
-    image_matrix.push(matrix_line)
-    matrix_line = []
-  }
-  img.clear();
-  console.log(image_matrix)
-  return image_matrix;
 }
